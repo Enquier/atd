c32700b91c74f3474777823dfd1446a36391365a
cgaley <cgaley@umich.edu>
2015-08-27 13:46:26 -0500

    module messup

--- salt_states/salt/_modules/nminc_install.py
+++ salt_states/salt/_modules/nminc_install.py
@@ -1,3 +1,5 @@
+import pexpect
+
 def teamwork (lic_dir,tw_dir):
 	inst = pexpect.spawn('%s/bin/teamwork_server -key:%s' % (tw_dir, lic_dir))
 	inst.expect('.\'I agree\':')
--- salt_states/salt/_modules/nminc_utils.py
+++ salt_states/salt/_modules/nminc_utils.py
@@ -1,5 +1,4 @@
 import re
-import pexpect
 
 def strsplit (string,char=' '):
 	try:
