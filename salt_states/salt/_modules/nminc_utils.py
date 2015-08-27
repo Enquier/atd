eb26608af4096b434f10f52a8c2ea2730e005774
cgaley <cgaley@umich.edu>
2015-08-27 09:40:35 -0500

    mod util

--- salt_states/salt/_modules/nminc_utils.py
+++ salt_states/salt/_modules/nminc_utils.py
@@ -12,5 +12,5 @@ def install_teamwork (lic_dir,tw_dir):
 	inst = pexpect.spawn('%s/bin/teamwork_server -key:%s' % (tw_dir, lic_dir))
 	inst.expect('.\'I agree\':')
 	inst.sendline('I agree')
-	inst.expect('Apply the key? (y/n)')
+	inst.expect('\nApply the key? (y/n)')
 	inst.sendline('y')
