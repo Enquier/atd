27e391af43906dd53ec8172373328318ec6968a0
cgaley <cgaley@umich.edu>
2016-02-08 14:37:15 -0600

    fixing try

--- salt_states/salt/_modules/nminc_install.py
+++ salt_states/salt/_modules/nminc_install.py
@@ -4,24 +4,28 @@ def teamwork (lic_dir,tw_dir):
 	inst = pexpect.spawn('%s/bin/teamwork_server_nogui -key:%s' % (tw_dir, lic_dir))
     try:
         i = inst.expect(['.\'I agree\':','JAVA_HOME ok: /usr/lib/jvm/java')
+    except EOFError:
+        return (False, str(inst))
+    else:
         if i==0:
             print('Key application required')
             inst.sendline('I agree')
             try:
                 j = inst.expect('.Apply the key\? \(y/n\)')
+            except:
+                print("There was something wrong with the key")
+                print(str(inst))
+                return False
+            else:
                 if j==0:
                     inst.sendline('y')
                     return (True, 'Key Successfully Applied')
                 else:
                     return (False, str(inst))
-            except:
-                print("There was something wrong with the key")
-                print(str(inst))
-                return False
+            
         elif i==1:
             return (True, 'Key has already been applied')
-    except EOFError:
-        return (False, str(inst))
+    
 
 
 def gen_expect (name,pattern,response):
